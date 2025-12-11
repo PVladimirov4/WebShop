b5ac32149905ad58dc3935f8ea790cbb940d110f
d.peichev2021@pmg-pernik.com <d.peichev2021@pmg-pernik.comgit config --global user.email d.peichev2021@pmg-pernik.com>
2025-12-11 13:13:20 +0200

    task 3

---
+++ WebShopApp/WebShopApp.Core/Contracts/ICategoryService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+using WebShopApp.Core.Contracts;
+using WebShopApp.Infrastructure.Data;
+using WebShopApp.Infrastructure.Data.Domain;
+
+namespace WebShopApp.Core.Contracts
+{
+    public interface ICategoryService
+    {
+        List<Category> GetCategories();
+        Category GetCategoryById(int categoryId);
+        List<Product> GetProductsByCategory(int categoryId);
+    }
+}
