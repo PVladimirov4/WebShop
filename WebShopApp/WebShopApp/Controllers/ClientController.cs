a1d924507053dbf683c1095653dd6bc046c02392
d.peichev2021@pmg-pernik.com <d.peichev2021@pmg-pernik.comgit config --global user.email d.peichev2021@pmg-pernik.com>
2025-12-18 13:09:13 +0200

    task 6 bez poslednite

---
+++ WebShopApp/WebShopApp/Controllers/ClientController.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+using WebShopApp.Infrastructure.Data.Domain;
+using WebShopApp.Models.Client;
+
+namespace WebShopApp.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ClientController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        // GET: ClientController
+        public async Task<IActionResult> Index()
+        {
+            // Get all users and map them to ClientIndexVM objects
+            var allUsers = this._userManager.Users
+                .Select(u => new ClientIndexVM
+                {
+                    Id = u.Id,
+                    UserName = u.UserName,
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    Address = u.Address,
+                    Email = u.Email,
+                })
+                .ToList();
+
+            // Get IDs of all users in the "Administrator" role
+            var adminIds = (await _userManager.GetUsersInRoleAsync("Administrator"))
+                .Select(a => a.Id)
+                .ToList();
+
+            // Set IsAdmin property for each user
+            foreach (var user in allUsers)
+            {
+                user.IsAdmin = adminIds.Contains(user.Id);
+            }
+
+            // Filter out administrators and sort by username
+            var users = allUsers
+                .Where(x => x.IsAdmin == false)
+                .OrderBy(x => x.UserName)
+                .ToList();
+
+            // Return the list to the view
+            return this.View(users);
+        }
+
+        // GET: ClientController/Details/5
+        public ActionResult Details(string id)
+        {
+            return View();
+        }
+
+        // GET: ClientController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClientController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClientController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ClientController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClientController/Delete/5
+        public ActionResult Delete(string id)
+        {
+            var user = this._userManager.Users.FirstOrDefault(x => x.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            ClientDeleteVM userToDelete = new ClientDeleteVM()
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Address = user.Address,
+                Email = user.Email,
+                UserName = user.UserName
+            };
+
+            return View(userToDelete);
+        }
+
+        // POST: ClientController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(ClientDeleteVM bindingModel)
+        {
+            string id = bindingModel.Id;
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            IdentityResult result = await _userManager.DeleteAsync(user);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Success");
+            }
+
+            return NotFound();
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
